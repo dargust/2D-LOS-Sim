3e52b4f8b6c0c0377e2bc74fa01bb34850379e1b
Dacus <Dacus@Test.me>
2024-09-30 21:38:56 +0100

    Slight mod to launcher build

--- build.bat
+++ build.bat
@@ -1,5 +1,9 @@
 @echo off
-echo Running pyinstaller build..
+echo Running pyinstaller build for launcher..
 pyinstaller launcher.spec
 echo Build finished, moving built exe to main dir..
 move .\dist\launcher.exe launcher.exe
+echo Running pyinstaller build for main executable..
+pyinstaller 2DLOS.spec
+echo DONE!!
+pause
